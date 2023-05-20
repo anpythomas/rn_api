1d7daf081dedb658a318b9c52bdb184fa3ec75cd
Andy <andythomas.py@gmail.com>
2023-05-20 15:43:02 -0500

    Added api json resonse

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+const savage = {
+    'age': 29,
+    'birthName': 'Shéyaa Bin Abraham-Joseph',
+    'birthLocation': 'London, England' 
+}
+
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api', (request, response) => {
+    response.json(savage)
 })
 
 app.listen(PORT, () => {
