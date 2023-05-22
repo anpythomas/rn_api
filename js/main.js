e4f23ac8850a83faf7d0c49c949b3d851ef64a50
Andy <andythomas.py@gmail.com>
2023-05-22 16:18:44 -0500

    Added try/catch to mainls

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 document.querySelector('button').addEventListener('click', logJSONData);
 
 async function logJSONData() {
-    const response = await fetch("https://rn-api-o93f.onrender.com/api/drake");
-    const jsonData = await response.json();
-    console.log(jsonData);
+    const name = document.querySelector('input').value
+    try {
+        const response = await fetch(`https://rn-api-o93f.onrender.com/api/${name}`);
+        const jsonData = await response.json();
+        console.log(jsonData);
+        document.querySelector('h2').innerText = jsonData.birthName
+    } catch (err) {
+        console.log(err)
+    }
+
     }
 
